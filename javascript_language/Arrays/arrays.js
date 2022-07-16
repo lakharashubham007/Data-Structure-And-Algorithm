abd796bc60d77e291ce9334f4e22fd3e08f7ce17
lakharashubham007 <lakharashubham623@gmail.com>
2022-07-16 16:39:01 +0530

    Day1_16July_ArrayIntro

---
+++ javascript_language/Arrays/arrays.js
@@ -0,0 +1,30 @@
+console.log(' **************************************************');
+ 
+//Array initialize , declare , and access
+
+
+
+let fruits = ["Apple","Banana","Grapse","Date","Mango"];
+
+console.log(fruits); 
+console.log(fruits[2]);
+console.log(fruits.length);
+console.log(' **************************************************');
+/* ******************************************* */
+
+let arr = ["Apple", { name : 'Shubham'}, true,  function() { console.log('hello'); } ]
+
+console.log(arr[0]);
+arr[3]();
+
+console.log(' **************************************************');
+/* ************************************************** */
+
+let fruitswala = ["Apple","Banana","Grapse","Date","Mango"];
+
+//last element print karwana ho to 
+console.log(fruitswala[fruitswala.length-1]);
+console.log(fruitswala.at(-2));
+
+console.log(' **************************************************');
+/* ************************************************** */
