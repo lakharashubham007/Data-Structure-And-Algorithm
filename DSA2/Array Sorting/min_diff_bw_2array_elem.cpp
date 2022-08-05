1d2650ad19a56662783dfdfe539f2d2e74dac796
lakharashubham007 <lakharashubham623@gmail.com>
2022-08-05 05:42:25 +0530

    sortinggfg

---
+++ DSA2/Array Sorting/min_diff_bw_2array_elem.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minDiff(int n, vector<int> arr){
+
+     // Sort array in non-decreasing order
+   sort(arr.begin(), arr.end());
+ 
+   // Initialize difference as infinite
+   int diff = INT_MAX;//INT_MAX is a macro that specifies that an integer variable cannot store any value beyond this limit. 
+ 
+   // Find the min diff by comparing adjacent
+   // pairs in sorted array
+   for (int i=0; i<n-1; i++)
+      if (arr[i+1] - arr[i] < diff)
+          diff = arr[i+1] - arr[i];
+ 
+   // Return min diff
+   return diff;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(auto &el: arr){
+        cin>>el;
+    }
+    int ans = minDiff(n, arr);
+    cout << ans << "\n";
+}
