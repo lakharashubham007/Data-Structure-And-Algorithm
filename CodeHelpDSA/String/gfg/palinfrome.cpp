bb8df048154e89887c295dfcd9036b455de017ae
lakharashubham007 <lakharashubham623@gmail.com>
2023-01-07 15:15:24 +0530

    string

---
+++ CodeHelpDSA/String/gfg/palinfrome.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+bool isPal(string &str){
+    int begin =0;
+    int end = str.length()-1;
+
+    while(begin < end) {
+        if(str[begin] != str[end])
+        return false;
+        begin++;
+        end--;
+    }
+    return true;
+}
+
+int main() {
+
+    string str = "bob";
+    
+    isPal(str);
+
+   if(isPal(str) == true){
+    cout<<"String is palindrome!"<<" ";
+    }else{
+        cout<<"String is not palindrome!"<<endl;
+    }
+
+    return 0;
+}
